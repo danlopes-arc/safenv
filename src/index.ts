72be0ba76ab2e3550081d9d3b4a8689543813d41
Daniel <danlpsarcanjo@gmail.com>
2021-07-20 19:49:23 -0400

    Implement env()

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-export function greet(text: string): string {
-  return `Hello, ${text}`
+import {ClassConstructor} from "class-transformer";
+import {Env} from "./Env";
+
+const envs = new Map<any, Env<any>>()
+
+export function env<T extends Record<string, any>>(envClass: ClassConstructor<T>): Env<T> {
+  let env = envs.get(envClass);
+  if (env) {
+    return env as Env<T>;
+  }
+  env = new Env(envClass);
+  envs.set(envClass, env);
+  return env;
 }
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,6 +1,67 @@
-import {greet} from "../src";
+import 'reflect-metadata'
+import {Expose} from "class-transformer";
+import {IsInt, IsNumber, IsString} from "class-validator";
+import {env} from '../src'
 
-it('should greet', function () {
-  const greeting = greet('Afonso')
-  expect(greeting).toBe('Hello, Afonso')
-});
+class TestEnv1 {
+  @Expose()
+  @IsString()
+  DB_NAME!: string
+
+  @Expose()
+  @IsNumber()
+  @IsInt()
+  PORT!: number
+}
+
+class TestEnv2 {
+  @Expose()
+  @IsString()
+  CLIENT_KEY!: string
+
+  @Expose()
+  @IsNumber()
+  @IsInt()
+  COUNT!: number
+}
+
+const originalProcessEnv = {...process.env}
+
+const validVariables = {
+  DB_NAME: 'test-name',
+  PORT: '3000',
+  CLIENT_KEY: 'client-key',
+  COUNT: '7'
+}
+
+beforeEach(() => {
+  process.env = {
+    ...originalProcessEnv,
+    ...validVariables
+  }
+})
+
+
+describe('env()', () => {
+  it('should return Env class with correct variables', function () {
+    const testEnv1 = env(TestEnv1)
+    const testEnv2 = env(TestEnv2)
+
+    expect(testEnv1.all()).toEqual({
+      DB_NAME: 'test-name',
+      PORT: 3000,
+    })
+
+    expect(testEnv2.all()).toEqual({
+      CLIENT_KEY: 'client-key',
+      COUNT: 7
+    })
+  });
+
+  it('should return same Env class', function () {
+    const firstTestEnv = env(TestEnv1)
+    const secondTestEnv = env(TestEnv1)
+
+    expect(firstTestEnv).toBe(secondTestEnv)
+  });
+})
